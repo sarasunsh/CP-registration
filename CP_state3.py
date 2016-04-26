e06b89cf8a4f865ac73e615648da08db5e109b54
SUNSHINE <sarasunsh@gmail.com>
2016-04-26 12:25:39 -0400

    Fixed bug with incrementing date

--- CP_state3.py
+++ CP_state3.py
@@ -8,9 +8,7 @@
 import requests
 import re
 import sys
-from datetime import date
-from datetime import datetime
-from datetime import time
+from datetime import date, datetime, time, timedelta
 from bs4 import BeautifulSoup
 
 # Local Package Imports
@@ -18,7 +16,7 @@ from CP_pull import CP_get, CP_faves
 from countdown import countdown
 
 # Paste cookies from Chrome here (as unbroken string)
-raw_cook = ''
+raw_cook = '__cfduid=d6e427245483d3e8ebe91b77c89b0edf21403099090103; km_ai=AsqNnwuqhKFg0io8vbrqZEZf2M4%3D; km_lv=x; TrackJS=5f317f37-5b03-4966-9bac-cb8a5ab83e01; _hp2_id.1165694113=2478143952259425.0739197400.2191247799; km_uq=; optimizelyEndUserId=oeu1422464448920r0.9521982800215483; __ar_v4=4FVLTQMN6JG6JOZ7FW6XXC%3A20150225%3A204%7CLMDV4NAO45EOZLWOSAQ5LJ%3A20150225%3A253%7CKJDLOVCT6VF4TBYXFFRSW3%3A20150225%3A253%7CLX4JV2JFOJEQBCLI4P74B5%3A20150315%3A8; sailthru_hid=9fcf94234a5f45ec8b2a806ffd511e3f548099032912ffa80a8b57e07101ad1fa4e325a0524db63ce59d0c40; _hp2_id.3868021954=6349802044283751.3780338889.1752130478; __insp_slim=1458137781396; __insp_wid=324523599; __insp_nv=true; __insp_ref=d; __insp_targlpu=https%3A%2F%2Fclasspass.com%2F; __insp_targlpt=ClassPass; __insp_norec_sess=true; cpExperimentId=56; cpUtmValues=%7B%22initial_url%22%3A%22%2F%22%7D; cpVisitorId=654614771553664e8d2dbf; cpGuestView=6; cpBasePlan=15; cpCohort=false_base_2; _gat=1; cpVisitCount=true; bounceClientVisit1678v={"lp":"https%3A%2F%2Fclasspass.com%2Fstart","r":""}; cpUser=NTAwOGI5ZDBiNjRkZWQxYWU0OTNjYTNiNDMzNDkzZmI4MTBlMThiOWFhNTMwNTE2Nzk4MzJmMzBlNzRjNGNjMA%3D%3D%7CMzcxODE%3D; classpass=ce0c4caea8c7d4360d75091660be5b21; cpUserType=subscribed; optimizelySegments=%7B%222331020308%22%3A%22false%22%2C%222347780571%22%3A%22gc%22%2C%222362360229%22%3A%22direct%22%2C%223537322437%22%3A%22none%22%7D; optimizelyBuckets=%7B%223544160422%22%3A%220%22%7D; _ga=GA1.2.345824304.1403099091; __srret=1; mp_13a079f3c862893a971ff2215ef81c76_mixpanel=%7B%22distinct_id%22%3A%20%2237181%22%2C%22%24initial_referrer%22%3A%20%22%24direct%22%2C%22%24initial_referring_domain%22%3A%20%22%24direct%22%2C%22utm_source%22%3A%20%22classpass%22%2C%22utm_medium%22%3A%20%22email%22%2C%22utm_campaign%22%3A%20%22Email%22%2C%22utm_content%22%3A%20%228212%22%2C%22msa_id%22%3A%20%221%22%2C%22msa_name%22%3A%20%22New%20York%20Metro%22%2C%22user_type%22%3A%20%22subscribed%22%2C%22count_lifetime_visits%22%3A%209%2C%22first_visit_date%22%3A%20%222015-12-23%22%2C%22guest_view%22%3A%20%22false_base_2%22%2C%22user_id%22%3A%20%2237181%22%2C%22session_start_count%22%3A%2012%2C%22app_platform%22%3A%20%22web%22%2C%22device_type%22%3A%20%22desktop%22%2C%22%24search_engine%22%3A%20%22google%22%7D; _fbuy=065a0850555f0e51195f080505140c56070b150d030f0d19050c5b0c055d5e0c000d0b01; bounceClientVisit1678={"sid":3,"fvt":1455722728,"vid":1461685609982887,"ao":2,"as":0,"vpv":2,"d":"d","lp":"https%3A%2F%2Fclasspass.com%2Fstart","r":"","cvt":1461685609,"gcr":63,"m":0,"did":"6300588047857816873","lvt":1461685615,"v":{"user_email":false,"zip_code":false,"ever_logged_in":true,"map_studios_count":0,"map_city_name":0,"user_type":"subscribed","data_msa_city_name":"New York Metro"},"campaigns":{"254724":{"lvt":1458052717,"lavid":1,"la":1458052717,"fsa":1457971052,"as":1,"ao":2}},"hc_home":1}; _fbuy_buckets=%7B%22bsy-f4y%22%3A%5B22716%2C1461685588971%5D%7D; __zlcmid=U1eXk35cY2NdnN; optimizelyPendingLogEvents=%5B%5D; mp_mixpanel__c=4'
  
 
 class ClassPass(object):
@@ -46,21 +44,23 @@ class ClassPass(object):
             studio_dict[url[-1]] = venue_id
 
 
-        ##TO DO: figure out better way to handle this? also confirm that a day < 10 needs leading zero added
+        # Get next week's date
+        #TO DO: this has a bug. You can't just add 6, because if it's towards the end of the month, it will get bumped
         # today_date = time.strftime("20%y-%m-%d")
         now = datetime.now()
-        if now.month < 10:
-            month = "0"+str(now.month)
+        next_week = now + timedelta(days=6)
+        if next_week.month < 10:
+            month = "0"+str(next_week.month)
         else:
-            month = str(now.month)
-        if now.day < 10:
-            day = "0"+str(now.day + 6)
+            month = str(next_week.month)
+        if next_week.day < 10:
+            day = "0"+str(next_week.day)
         else:
-            day = str(now.day + 6)
-        next_week = str(now.year)+"-"+month+"-"+day
+            day = str(next_week.day)
+        next_week_s = str(next_week.year)+"-"+month+"-"+day
 
         self.studio_dict = studio_dict
-        self.next_week = next_week
+        self.next_week_s = next_week_s
         self.cookies = COOKIES
         self.classes_in_cart = []
         self.display_studios()
@@ -84,7 +84,7 @@ class ClassPass(object):
     def display_classes(self):
 
         # Feed venue_id into function to retrieve html 
-        studio_html = CP_get(self.venue_id, self.next_week, self.cookies)
+        studio_html = CP_get(self.venue_id, self.next_week_s, self.cookies)
 
         # Use BeautifulSoup to pull link, title, and time information for classes 
         soup = BeautifulSoup(studio_html, "lxml")
@@ -112,26 +112,26 @@ class ClassPass(object):
             print j, class_names[j], str(start_times[j])+" - "+str(end_times[j])
  
         # Prompt for user input
-        self.class_prompt = input("Enter 1 if you would like to add a class to cart, 2 if you would like to look at the cart, and 3 if you would like to return to favorites: ")
-        if self.class_prompt == 1:
+        class_prompt = input("Enter 1 if you would like to add a class to cart, 2 if you would like to look at the cart, and 3 if you would like to return to favorites: ")
+        if class_prompt == 1:
             
-            self.class_num = input("Please enter the number of the class you'd like to add to the cart: ")
+            class_num = input("Please enter the number of the class you'd like to add to the cart: ")
             
             # Set class details for selected class
-            self.sched_id = sched_ids[self.class_num]
-            self.class_id = class_ids[self.class_num]
-            self.class_name = class_names[self.class_num]
-            self.class_time = str(start_times[self.class_num])+"-"+str(end_times[self.class_num])
-            class_deets = [self.sched_id, self.venue_id, self.class_id, self.class_name, self.venue_name, self.class_time]
+            sched_id = sched_ids[class_num]
+            class_id = class_ids[class_num]
+            class_name = class_names[class_num]
+            class_time = str(start_times[class_num])+"-"+str(end_times[class_num])
+            class_deets = [sched_id, self.venue_id, class_id, class_name, self.venue_name, class_time]
             
             # Add selected class to cart
             self.classes_in_cart.append(class_deets)
             self.display_cart()
 
-        elif self.class_prompt == 2:
+        elif class_prompt == 2:
             self.display_cart()
 
-        elif self.class_prompt == 3:
+        elif class_prompt == 3:
             self.display_studios()
 
     # Print the details for classes currently in cart and allow the user to modify the cart content
